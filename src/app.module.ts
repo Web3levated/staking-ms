9c9753a4a25ab486dc76818a8fa9f156b96a6d83
Louie <louis.miller@freaksnguilds.com>
2022-09-21 13:00:07 -0700

    nestJs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,39 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ethers } from 'ethers';
-import { CoinchainStaking__factory } from 'typechain';
+import { CoinchainStaking__factory, CoinchainToken__factory } from 'typechain';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 const coinchainStakingFactory = {
   provide: "CoinchainStaking",
   useFactory: () => {
-    const provider = new ethers.providers.JsonRpcProvider("provider");
-    const contractAddress = "contractAddress";
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const contractAddress = process.env.COINCHAIN_STAKING_ADDRESS;
     return CoinchainStaking__factory.connect(contractAddress, provider);
   }
 }
 
+const coinchainTokenFactory = {
+  provide: "CoinchainToken",
+  useFactory: () => {
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const contractAddress = process.env.COINCHAIN_TOKEN_ADDRESS;
+    return CoinchainToken__factory.connect(contractAddress, provider);
+  }
+}
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    coinchainStakingFactory
+    coinchainStakingFactory,
+    coinchainTokenFactory
   ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,13 +2,16 @@ import { Inject, Injectable } from '@nestjs/common';
 import { time } from 'console';
 import { PopulatedTransaction } from 'ethers';
 import { Chain, EthersBridge, FireblocksSDK } from 'fireblocks-defi-sdk';
-import { CoinchainStaking } from 'typechain';
+import { CoinchainStaking, CoinchainToken } from 'typechain';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject("CoinchainStaking") 
-    private readonly coinchainStaking: CoinchainStaking
+    private readonly coinchainStaking: CoinchainStaking,
+
+    @Inject("CoinchainToken")
+    private readonly coinchainToken: CoinchainToken
   ) {}
 
   getHello(): string {
@@ -16,29 +19,35 @@ export class AppService {
   }
 
   async createStakes() : Promise<string> {
-    const apiSecret = "secret";
-    const apiKey = "key";
-    const fireblocksApiBaseUrl = "url";
-    const vaultAccount = "vaultAccount";
+    const apiSecret = process.env.FIREBLOCKS_API_SECRET_PATH;
+    const apiKey = process.env.FIREBLOCKS_API_KEY;
+    const fireblocksApiBaseUrl = process.env.FIREBLOCKS_API_BASE_URL;
+    const vaultAccount = process.env.FIREBLOCKS_CCH_VAULT;
     const fireblocksApiClient = new FireblocksSDK(apiSecret, apiKey, fireblocksApiBaseUrl);
-    const bridge = new EthersBridge({
+    // const bridge = new EthersBridge({
+    //   fireblocksApiClient,
+    //   vaultAccountId: vaultAccount,
+    //   externalWalletId: this.coinchainStaking.address,
+    //   chain: Chain.ROPSTEN
+    // })
+    // const deposit: CoinchainStaking.DepositStruct = {
+    //   depositId: 1,
+    //   data: {
+    //     user: "0xBEf62fCa4E7bC887C295db13Bc4c81D0c3B786eF",
+    //     amount: 100,
+    //     yieldConfigId: 1,
+    //     depositTime: Date.now() / 1000
+    //   }
+    // }
+    // const populatedTransaction: PopulatedTransaction = await this.coinchainStaking.populateTransaction.deposit([deposit]);
+    // const res  = await bridge.sendTransaction(populatedTransaction);
+    // const txHash = await bridge.waitForTxHash(res.id);
+    const tokenBridge = new EthersBridge({
       fireblocksApiClient,
       vaultAccountId: vaultAccount,
-      externalWalletId: this.coinchainStaking.address,
-      chain: Chain.ROPSTEN
-    })
-    const deposit: CoinchainStaking.DepositStruct = {
-      depositId: 1,
-      data: {
-        user: "0xBEf62fCa4E7bC887C295db13Bc4c81D0c3B786eF",
-        amount: 100,
-        yieldConfigId: 1,
-        depositTime: Date.now() / 1000
-      }
-    }
-    const populatedTransaction: PopulatedTransaction = await this.coinchainStaking.populateTransaction.deposit([deposit]);
-    const res  = await bridge.sendTransaction(populatedTransaction);
-    const txHash = await bridge.waitForTxHash(res.id);
+      externalWalletId: this.coinchainToken.address,
+      chain: Chain.GOERLI
+    });
     return "createStakes called";
   }
 }
