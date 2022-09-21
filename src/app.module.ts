f3b154b70bf22246f2cfffd3cbd9e3ae99ac82dc
Louie <louis.miller@freaksnguilds.com>
2022-09-21 10:08:53 -0700

    experimenting with fireblocks sdk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ethers } from 'ethers';
+import { CoinchainStaking__factory } from 'typechain';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+const coinchainStakingFactory = {
+  provide: "CoinchainStaking",
+  useFactory: () => {
+    const provider = new ethers.providers.JsonRpcProvider("provider");
+    const contractAddress = "contractAddress";
+    return CoinchainStaking__factory.connect(contractAddress, provider);
+  }
+}
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    coinchainStakingFactory
+  ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { time } from 'console';
+import { PopulatedTransaction } from 'ethers';
+import { Chain, EthersBridge, FireblocksSDK } from 'fireblocks-defi-sdk';
+import { CoinchainStaking } from 'typechain';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @Inject("CoinchainStaking") 
+    private readonly coinchainStaking: CoinchainStaking
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createStakes() : Promise<string> {
+    const apiSecret = "secret";
+    const apiKey = "key";
+    const fireblocksApiBaseUrl = "url";
+    const vaultAccount = "vaultAccount";
+    const fireblocksApiClient = new FireblocksSDK(apiSecret, apiKey, fireblocksApiBaseUrl);
+    const bridge = new EthersBridge({
+      fireblocksApiClient,
+      vaultAccountId: vaultAccount,
+      externalWalletId: this.coinchainStaking.address,
+      chain: Chain.ROPSTEN
+    })
+    const deposit: CoinchainStaking.DepositStruct = {
+      depositId: 1,
+      data: {
+        user: "0xBEf62fCa4E7bC887C295db13Bc4c81D0c3B786eF",
+        amount: 100,
+        yieldConfigId: 1,
+        depositTime: Date.now() / 1000
+      }
+    }
+    const populatedTransaction: PopulatedTransaction = await this.coinchainStaking.populateTransaction.deposit([deposit]);
+    const res  = await bridge.sendTransaction(populatedTransaction);
+    const txHash = await bridge.waitForTxHash(res.id);
+    return "createStakes called";
   }
 }
